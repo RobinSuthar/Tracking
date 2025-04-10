366dc59bc05eba915695b409469905a0e272b6ba
RobinSuthar <robinsuthar.rs07@gmail.com>
2025-04-10 02:15:47 -0600

    Fixing issiues

--- app/api/tracks/[id]/route.ts
+++ app/api/tracks/[id]/route.ts
@@ -3,7 +3,10 @@ import { PrismaClient } from "@/app/generated/prisma";
 
 const prisma = new PrismaClient();
 
-export async function GET({ params }: { params: Promise<{ id: string }> }) {
+export async function GET(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
   const { id } = await params; // 'a', 'b', or 'c'
   const providedId = parseInt(id);
   const user = await prisma.tracks.findFirst({
